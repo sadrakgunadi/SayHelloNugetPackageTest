ae758749a159294ae5369f321dd19a135a4c00a3
Sadrak Gunadi <37262959+sadrakgunadi@users.noreply.github.com>
2019-01-25 22:41:50 +0700

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,5 @@
+Hi,
+
+This is test packages for NuGet
+
+Sadrak Gunadi
---
+++ sayHello.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SayHello
+{
+    /// <summary>
+    /// Say Hello Portable Library Test
+    /// </summary>
+    public static class sayHello
+    {
+        /// <summary>
+        /// Method Test
+        /// </summary>
+        /// <param name="yourName">your name</param>
+        /// <returns></returns>
+        public static string Hi(string yourName)
+        {
+            return $"Hi, {yourName}";
+        }
+    }
+}
